1e61285829539fb8cd250256b75de68afde29d71
lynjan <lynjan@163.com>
2020-04-22 13:46:49 +0800

    add helloworld

---
+++ HelloWorld/HelloWorld.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2020-04-21T13:40:08
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = HelloWorld
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
---
+++ HelloWorld/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //QLabel label("Hello world");
+    //label.show();
+
+    //QLabel *label = new QLabel("Hello world");
+    //label->show();
+    //label->setAttribute(Qt::WA_DeleteOnClose);
+
+    //QPushButton button("Quit");
+    //QObject::connect(&button, &QPushButton::clicked, &QApplication::quit);
+    //button.show();
+
+    QPushButton *button = new QPushButton("Quit");
+    QObject::connect(button, &QPushButton::clicked, &QApplication::quit);
+    button->show();
+    button->setAttribute(Qt::WA_DeleteOnClose);
+
+
+    return a.exec();
+}
