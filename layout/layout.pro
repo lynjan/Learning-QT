2d127fd432067b8e21fe84c9759e1d8e627b0241
lynjan <lynjan@163.com>
2020-04-22 13:54:19 +0800

    add layout

---
+++ layout/layout.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2020-04-22T09:50:53
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = layout
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
---
+++ layout/main.cpp
@@ -0,0 +1,30 @@
+﻿#include "widget.h"
+#include <QApplication>
+#include <QSlider>
+#include <QSpinBox>
+#include <QLayout>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.setWindowTitle("Enter your age");
+
+    QSpinBox *spinBox = new QSpinBox(&w);
+    QSlider *slider = new QSlider(Qt::Horizontal, &w);
+    spinBox->setRange(0, 130);
+    slider->setRange(0, 130);
+
+    QObject::connect(slider, &QSlider::valueChanged, spinBox, &QSpinBox::setValue);
+    void (QSpinBox:: *spinBoxSignal)(int) = &QSpinBox::valueChanged;
+    QObject::connect(spinBox, spinBoxSignal, slider, &QSlider::setValue);
+    spinBox->setValue(35);
+
+    QHBoxLayout *layout = new QHBoxLayout(&w);
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+    w.setLayout(layout);
+    w.show();
+
+    return a.exec();
+}
